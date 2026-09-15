6f632f648d10b4e6530020b7bc86e925cb881abd
smisei-lmz <exlce_lmz@163.com>
2026-09-15 19:23:05 +0800

    day9_final

---
+++ cpp/build/CMakeFiles/trt_example.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/smisei/Yolov11-ONNX-TensorRT/cpp/main.cpp" "CMakeFiles/trt_example.dir/main.cpp.o" "gcc" "CMakeFiles/trt_example.dir/main.cpp.o.d"
+  "/home/smisei/Yolov11-ONNX-TensorRT/cpp/src/BufferManager.cpp" "CMakeFiles/trt_example.dir/src/BufferManager.cpp.o" "gcc" "CMakeFiles/trt_example.dir/src/BufferManager.cpp.o.d"
+  "/home/smisei/Yolov11-ONNX-TensorRT/cpp/src/TrtEngine.cpp" "CMakeFiles/trt_example.dir/src/TrtEngine.cpp.o" "gcc" "CMakeFiles/trt_example.dir/src/TrtEngine.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cpp/build/CMakeFiles/trt_example.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trt_example.dir/main.cpp.o"
+  "CMakeFiles/trt_example.dir/main.cpp.o.d"
+  "CMakeFiles/trt_example.dir/src/BufferManager.cpp.o"
+  "CMakeFiles/trt_example.dir/src/BufferManager.cpp.o.d"
+  "CMakeFiles/trt_example.dir/src/TrtEngine.cpp.o"
+  "CMakeFiles/trt_example.dir/src/TrtEngine.cpp.o.d"
+  "trt_example"
+  "trt_example.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/trt_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
